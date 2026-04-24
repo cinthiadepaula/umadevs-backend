93bd6e20138fc11e1b58f2ebcd75b6cb0787a31f
cinthiadepaula <cinthiade.paula@outlook.com>
2026-04-24 13:05:47 -0300

    up

--- src/controllers/lideradoController.js
+++ src/controllers/lideradoController.js
@@ -1,35 +1,6 @@
 const Liderado = require("../models/Liderado");
 const Igreja = require("../models/Igreja");
 
-exports.criarLiderado = async (req, res) => {
-  try {
-    const { nome, igrejaId } = req.body;
-
-    let igrejaDoLiderado = igrejaId;
-
-    if (req.user.tipo === "LOCAL") {
-      igrejaDoLiderado = req.user.igrejaId;
-    }
-
-    if (!igrejaDoLiderado) {
-      return res.status(400).json({
-        error: "Informe a igreja do liderado",
-      });
-    }
-
-    const liderado = await Liderado.create({
-      nome,
-      igrejaId: igrejaDoLiderado,
-      criadoPor: req.user.id,
-    });
-
-    res.status(201).json(liderado);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Erro ao cadastrar liderado" });
-  }
-};
-
 exports.listarLiderados = async (req, res) => {
   try {
     let filtro = {};
@@ -49,11 +20,12 @@ exports.listarLiderados = async (req, res) => {
     }
 
     const liderados = await Liderado.find(filtro)
-      .populate("igrejaId", "nome")
+      .populate("igrejaId", "nome regionalId")
       .populate("criadoPor", "nome email");
 
     res.json(liderados);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Erro ao listar liderados" });
   }
 };
--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -1,25 +1,6 @@
 const Pedido = require("../models/Pedido");
 const User = require("../models/User");
-
-exports.criarPedido = async (req, res) => {
-  try {
-    const { itens } = req.body;
-
-    if (!itens || itens.length === 0) {
-      return res.status(400).json({ error: "Informe os itens" });
-    }
-
-    const pedido = await Pedido.create({
-      liderId: req.user.id,
-      itens,
-    });
-
-    res.status(201).json(pedido);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Erro ao criar pedido" });
-  }
-};
+const Igreja = require("../models/Igreja");
 
 exports.listarPedidos = async (req, res) => {
   try {
@@ -30,21 +11,30 @@ exports.listarPedidos = async (req, res) => {
     }
 
     if (req.user.tipo === "REGIONAL") {
-      const lideres = await User.find({
+      const igrejas = await Igreja.find({
         regionalId: req.user.regionalId,
       });
 
-      const ids = lideres.map((l) => l._id);
+      const igrejasIds = igrejas.map((igreja) => igreja._id);
 
-      filtro.liderId = { $in: ids };
+      const lideres = await User.find({
+        igrejaId: { $in: igrejasIds },
+        tipo: "LOCAL",
+      });
+
+      const lideresIds = lideres.map((lider) => lider._id);
+
+      filtro.liderId = { $in: lideresIds };
     }
 
-    // GERAL vê tudo
-
-    const pedidos = await Pedido.find(filtro).populate("liderId", "nome email");
+    const pedidos = await Pedido.find(filtro).populate(
+      "liderId",
+      "nome email tipo igrejaId regionalId",
+    );
 
     res.json(pedidos);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Erro ao listar pedidos" });
   }
 };

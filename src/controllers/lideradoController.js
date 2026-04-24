bfa650dddc6e1a4d5188a5c985d1ec64e1786d79
cinthiadepaula <cinthiade.paula@outlook.com>
2026-04-24 14:44:32 -0300

    up

--- src/controllers/lideradoController.js
+++ src/controllers/lideradoController.js
@@ -5,26 +5,30 @@ exports.listarLiderados = async (req, res) => {
   try {
     let filtro = {};
 
-    console.log("USUÁRIO LOGADO:", req.user);
-
     if (req.user.tipo === "LOCAL") {
       filtro.igrejaId = req.user.igrejaId;
     }
 
     if (req.user.tipo === "REGIONAL") {
-      if (!req.user.regionalId) {
-        return res.status(403).json({
-          error: "Usuário regional sem regional vinculada",
-        });
-      }
-
       const igrejasDaRegional = await Igreja.find({
         regionalId: req.user.regionalId,
-      }).select("_id");
+      }).select("_id nome regionalId");
 
       const idsIgrejas = igrejasDaRegional.map((igreja) => igreja._id);
 
       filtro.igrejaId = { $in: idsIgrejas };
+
+      const liderados = await Liderado.find(filtro)
+        .populate("igrejaId", "nome regionalId")
+        .populate("criadoPor", "nome email tipo");
+
+      return res.json({
+        usuarioLogado: req.user,
+        igrejasDaRegional,
+        filtroUsado: filtro,
+        total: liderados.length,
+        liderados,
+      });
     }
 
     const liderados = await Liderado.find(filtro)
